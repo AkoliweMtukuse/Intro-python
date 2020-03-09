fd3eafa4c2661b36838cda72621fee2ceb68a676
unknown <akoliwemtukuse@gmail.com>
2020-03-09 02:40:43 -0700

    First commit

---
+++ Variable.py
@@ -0,0 +1,13 @@
+print('good morning') #This is an illustration of using the print keyword and displaying text
+print("I spent this weekend with my brother at home") #This was a further illustration of the print keyword and displaying text
+print("All we did was to play videos games") # example 2
+x=40 #determining the value of x
+y=55.5 #determining the value of y
+str_multiline="""I am working hard.\n I like what I am doing.\n It is fun doing this program.\n We at lifechoices are on fire"""
+print(x) #displaying the value of x
+print(y) #displaying the value of y
+print(str_multiline) #diplaying the four statements written
+r = 5 #determining the value of r
+pi = 3.14 #determining the value of pi
+a=pi*r**2 #multiplying pi and r 2 with the square root of 2
+print(a) #displaying the result of the sum
